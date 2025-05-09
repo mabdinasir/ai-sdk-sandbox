3ecc3c375d12657e9b7e9b8aeb4de98bdac5c1b7
Mohamed Abdinasir <mabdinasira@gmail.com>
2025-05-10 07:09:09 +1000

    add vscode settings to project

--- chatbot-ai-sdk/app/api/chat/route.ts
+++ chatbot-ai-sdk/app/api/chat/route.ts
@@ -1,17 +1,17 @@
-import {streamText} from 'ai';
-import {createOllama} from 'ollama-ai-provider';
+import { streamText } from "ai";
+import { createOllama } from "ollama-ai-provider";
 
 const ollama = createOllama();
 
 const model = ollama("mistral");
 
 export async function POST(req: Request) {
-	const {messages} = await req.json();
+    const { messages } = await req.json();
 
-	const result = streamText({
-		model: model,
-		messages,
-	});
+    const result = streamText({
+        model: model,
+        messages,
+    });
 
-	return result.toDataStreamResponse();
+    return result.toDataStreamResponse();
 }
